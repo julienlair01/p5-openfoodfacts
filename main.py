04c332464d2996a4cd11e7385273b49640540811
Julien Lair <julien.lair@gmail.com>
2020-10-06 15:29:26 +0200

    methods to extract categories from json

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+from pprint import pprint
+
+def read_categories_from_json():
+    with open('categories.json', 'r') as f:
+        categories = json.load(f)
+    return categories
+
+def filter_fr_categories(categories):
+    fr_categories = []
+    for i in range (len(categories['tags'])):
+        if "fr:" in categories['tags'][i]['id']:
+            fr_categories.append({'off_id': categories['tags'][i]['id'], 'name': categories['tags'][i]['name']})
+        else:
+            pass
+    return fr_categories
+
+
+
+raw_categories = read_categories_from_json()
+clean_fr_categories = filter_fr_categories(raw_categories)
+
+pprint(clean_fr_categories)
