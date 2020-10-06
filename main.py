923b6339e68617c34ba44984a21ca71289f85567
Julien Lair <julien.lair@gmail.com>
2020-10-06 16:23:02 +0200

    updated writing in db

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ from pprint import pprint
 def read_categories_from_json():
     with open('categories.json', 'r') as f:
         categories = json.load(f)
+    print('Categories extracted from json.')
     return categories
 
-def filter_fr_categories(categories):
-    fr_categories = []
+def filter_categories(categories):
+    filtered_categories = []
     for i in range (len(categories['tags'])):
         if "fr:" in categories['tags'][i]['id']:
-            fr_categories.append({'off_id': categories['tags'][i]['id'], 'name': categories['tags'][i]['name']})
+            filtered_categories.append({'off_id': categories['tags'][i]['id'], 'name': categories['tags'][i]['name'][3:]})
         else:
             pass
-    return fr_categories
-
+    print('Categories filtered, keeping FR only.')
+    return filtered_categories
 
 db = database.Database()
 
 raw_categories = read_categories_from_json()
-clean_fr_categories = filter_fr_categories(raw_categories)
+filtered_categories = filter_categories(raw_categories)
 
-for item in clean_fr_categories:
+db.connect_to_db()
+
+for item in filtered_categories:
     db.write_category(item['off_id'], item['name'])
 
+db.disconnect_from_db()
+
+print('Category table updated.')
 
